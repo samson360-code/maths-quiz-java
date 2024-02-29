ccb475a600b1775ecbf7efd49e700c148b2924cd
Disami <133263654+samson360-code@users.noreply.github.com>
2024-02-29 23:25:49 +0300

    Add files via upload

---
+++ maths quiz/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+     public static void printcard(){
+        System.out.print("╔══════════╗\n║          ║\n║          ║\n║          ║\n║          ║\n║          ║\n║          ║\n║          ║\n╚══════════╝");
+     }
+    public static void main(String[] args) throws Exception {
+        printcard();
+    }
+}
